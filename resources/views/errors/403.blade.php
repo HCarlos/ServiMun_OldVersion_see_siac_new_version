aee5b3bb2e4a9bf2ef4c52ee99cf10a7560da6c4
HCarlos <r0@tecnointel.mx>
2021-10-02 17:45:33 +0000

    AteMun - B5 | L7.30.4 Beta

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -2,4 +2,16 @@
 
 @section('title', __('Forbidden'))
 @section('code', '403')
-@section('message', __($exception->getMessage() ?: 'Forbidden'))
+@section('message')
+    <p>No tiene permisos para ingresar. Contacte al administrador</p>
+    <a class="dropdown-item" href="{{ route('logout') }}"
+       onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">
+        {{ __('Logout') }}
+    </a>
+
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+    </form>
+
+@endsection
--- resources/views/partials/left-sidebar-ciudadano.blade.php
+++ resources/views/partials/left-sidebar-ciudadano.blade.php
@@ -13,6 +13,7 @@
             </span>
         </a>
     @guest()
+
     @else()
         <!--- Sidemenu -->
         <ul class="metismenu side-nav">
