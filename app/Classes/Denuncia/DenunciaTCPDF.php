b7fd4449286b473c9133042ce0c2cd0efacf9323
HCarlos <r0@tecnointel.mx>
2022-01-20 00:40:36 +0000

    AteMun - B41-K | L7.30.4 Beta

--- app/Classes/Denuncia/DenunciaTCPDF.php
+++ app/Classes/Denuncia/DenunciaTCPDF.php
@@ -19,6 +19,7 @@ namespace App\Classes\Denuncia;
 
 use App\Traits\TCPDF\InitTrait;
 use Carbon\Carbon;
+use http\Env\Request;
 use TCPDF;
 use TCPDF_COLORS;
 use TCPDF_STATIC;
@@ -32,38 +33,50 @@ class DenunciaTCPDF extends TCPDF{
     protected $title       = "";
     public $folio          = 0;
     protected $date        = null;
+    public $FOLIO          = "";
 
     public function Header() {
-        $this->date = new Carbon( $this->timex );
-        $this->setY(15);
+
+        $style = array(
+            'border' => 0,
+            'vpadding' => 'auto',
+            'hpadding' => 'auto',
+            'fgcolor' => array(28,28,28),
+            'bgcolor' => false, //array(255,255,255)
+            'module_width' => 1, // width of a single module in points
+            'module_height' => 1 // height of a single module in points
+        );
+
+        $this->setY(7);
         $this->setX(5);
         $this->SetTextColor(64,64,64);
         $this->SetFillColor(212,212,212);
 
-        $this->Image(ATEMUN['logo_reportes_encabezado'],5,15,60,19);
-
-        $this->Cell(62,$this->alto,"","R",0,"L");
-        $this->Cell(2,$this->alto,"","",0,"L");
-        $this->SetFont(FONT_ARIALN,'',11);
-        $this->Cell(120,$this->alto,env("NOMBRE_EMPRESA"),"",1,"L");
-
-        $this->Cell(62,$this->alto,"","R",0,"L");
-        $this->Cell(2,$this->alto,"","",0,"L");
-        $this->SetFont(FONT_FREEMONO,'B',14);
-        $this->Cell(120,$this->alto,env('INFO_TWO'),"",1,"L");
+        $this->Image(ATEMUN['logo_reportes_encabezado'],5,7,60,19);
+        $this->write2DBarcode(url()->full(), 'QRCODE,H', 180, 5, 50, 50, $style, '');
 
         $this->Cell(62,$this->alto,"","R",0,"L");
         $this->Cell(2,$this->alto,"","",0,"L");
         $this->SetFont(FONT_ARIALN,'',10);
+        $this->Cell(120,$this->alto,env("NOMBRE_EMPRESA"),"",1,"L");
+
+        $this->Cell(62,$this->alto,"","R",0,"L");
+        $this->Cell(2,$this->alto,"","",0,"L");
+        $this->SetFont(FONT_FREEMONO,'B',11);
+        $this->Cell(120,$this->alto,env('INFO_TWO'),"",1,"L");
+
+        $this->Cell(62,$this->alto,"","R",0,"L");
+        $this->Cell(2,$this->alto,"","",0,"L");
+        $this->SetFont(FONT_ARIALN,'',9);
         $this->Cell(85,$this->alto,"REPORTE CIUDADANO","",1,"L");
         $this->SetFont(FONT_FREEMONO,'B',10);
+        $this->ln(10);
         $this->Cell(149,$this->alto,"","",0,"L");
         $this->Cell(22,$this->alto,"FOLIO: ","",0,"L");
-        $this->SetFont(FONT_DEJAVUSANSMONO,'B',10);
+        $this->SetFont(FONT_DEJAVUSANSMONO,'B',8);
         $this->SetTextColor(255,64,64);
-        $this->Cell(25,$this->alto,"DAC-".str_pad($this->folio,6,'0',STR_PAD_LEFT)."-".$this->date->format('y'),"",1,"R");
+        $this->Cell(25,$this->alto,$this->FOLIO,"",1,"R");
         $this->SetTextColor(64,64,64);
-
         $this->SetFont(FONT_FREEMONO,'B',10);
         $this->Cell(149,$this->alto,"","",0,"L");
         $this->Cell(22,$this->alto,"FECHA: ","",0,"L");
--- app/Http/Controllers/External/Denuncia/HojaDenunciaController.php
+++ app/Http/Controllers/External/Denuncia/HojaDenunciaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\External\Denuncia;
 use App\Classes\Denuncia\DenunciaTCPDF;
 use App\Http\Controllers\Controller;
 use App\Models\Denuncias\Denuncia;
+use BaconQrCode\Encoder\QrCode;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
@@ -15,20 +16,25 @@ class HojaDenunciaController extends Controller
 
 
     public function imprimirDenuncia($Id=0){
+
+//        require_once('tcpdf_include.php');
+
         $timex  = Carbon::now()->format('d-m-Y H:i:s');
+        $date = new Carbon( $timex );
         $folio  = $Id;
+        $FOLIO = "DAC-".str_pad($folio,6,'0',STR_PAD_LEFT)."-".$date->format('y');
         $alto   = 6;
 
         // dd(NOMBRE_EMPRESA);
 
         $pdf = new DenunciaTCPDF('','mm',array(215.9, 139.7), true, 'UTF-8', false);
+        $pdf->FOLIO = $FOLIO;
         $pdf->folio = $folio;
         $pdf->timex = $timex;
 
-        $certificate =  'file://' . getcwd() . "/signature/tcpdf.crt";
+//        $certificate =  'file://' . getcwd() . "/signature/tcpdf.crt";
 
-
-        //$certificate =  'file://' . getcwd() . "/signature/hirc711126jt0.cer";
+        $certificate =  'file://' . getcwd() . "/signature/hirc711126jt0.cer";
         $clave = "CH50Dev";
 
         if ( file_exists($certificate) ) {
@@ -41,18 +47,22 @@ class HojaDenunciaController extends Controller
         }
 
         $info = array(
-            'Name' => 'TCPDF',
-            'Location' => 'Office',
-            'Reason' => 'H. Ayuntamiento Constitucional del Municipio de Centro',
+            'Name' => 'SIAC',
+            'Location' => 'H. Ayuntamiento Constitucional del Municipio de Centro, Tabasco, MX',
+            'Reason' => 'Firma Digital de Documentos',
             'ContactInfo' => 'https://villahermosa.gob.mx',
         );
 
-        $pdf->setSignature($certificate, $certificate, $clave, '', 3, $info);
+//        $pdf->setSignature($certificate, $certificate, $clave, '', 2, $info);
+        //$pdf->SetProtection($permissions=array('print', 'copy'), $user_pass='CH50Dev', $owner_pass=null, $mode=1, $pubkeys=array(array('c' => $certificate, 'p' => array('print'))));
 
         $pdf->Init();
         $pdf->AddPage();
 
         $pdf->setCellPaddings(1, 1, 1, 1);
+
+// set image scale factor
+        $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
 
 // Linea 1
@@ -69,49 +79,124 @@ class HojaDenunciaController extends Controller
         $roles = $den->ciudadano->RoleNameStrArray;
         $username = $den->ciudadano->username;
         $html = ATEMUN['style']['denuncia'];
-        $html .= "<p>Estimado <bAzul>{$den->ciudadano->FullName}</bAzul> (<bChocolate>{$den->ciudadano->id}</bChocolate>, <bOrange>$username</bOrange>), su petición ha sido recibida y se iniciará el trámite pertinente. <br><br>";
+        $html .= "<p>Estimado <bAzul>C. {$den->ciudadano->FullName}</bAzul> (<bChocolate>{$den->ciudadano->id}</bChocolate>, <bOrange>$username</bOrange>), su petición ha sido recibida y se iniciará el trámite pertinente. <br><br>";
         $html .= "El <b>". NOMBRE_EMPRESA . "</b> agradece su colaboración y le garantiza confidencialidad y una pronta respuesta.  <br><br>";
-        $html .= "Fue atendido por <bVerde>{$den->creadopor->FullName}</bVerde>. <br><br>";
+        $html .= "Fue atendido por <bVerde>C. {$den->creadopor->FullName}</bVerde>. <br>";
         $html .= "</p>";
         $html .= "<span></span>";
         $html .= "<pCentrado>";
         $html .= env('INFO_TWO'). "<br>";
         $html .= "<span></span>";
-        $html .= env('INFO_THREE'). "<br>";
-        $html .= "<span></span>";
         $html .= "<a href='".env('INFO_FOUR')."'>".env('INFO_FOUR'). "</a>";
         $html .= "</pCentrado>";
 
-        // $certificate = public_path() . '/signature/hirc711126jt0.cer';
+        $datos = $den->id.'|'.$pdf->FOLIO.'|'.$pdf->timex.'|'.$den->ciudadano->id.'|'.$den->ciudadano->username.'|'.$den->ciudadano->FullName.'|'.$den->creadopor->id.'|'.$den->creadopor->username.'|'.$den->creadopor->FullName.'|'.$den->dependencia_id.'|'.$den->dependencia_id.'|'.$den->servicio_id.'|'.$den->estatu_id;
 
-        //$certificate = 'file://' .getcwd() . "/signature/hirc711126jt0.cer";
+        $pdf->WriteHTMLCell(195,$alto,10,$pdf->getY(),$html,0,0);
+
+// guardar el mensaje en un archivo
+        $mensaje = public_path() . "/signature/mensaje.txt";
+        $firmado = public_path() . "/signature/firmado.txt";
+        $key_pem = public_path() . "/signature/Claveprivada_FIEL_HIRC711126JT0_20211206_140329.pem";
+        $pem     = public_path() . "/signature/hirc711126jt0.pem";
+        $fp = fopen(public_path() . "/signature/mensaje.txt", "w");
+        fwrite($fp, $datos);
+        fclose($fp);
+
+        $key=$key_pem;
+        $fp = fopen($key, "r");
+        $priv_key = fread($fp, 8192);
+
+        $pkeyid = openssl_get_privatekey($priv_key);
+
+        if (openssl_sign($mensaje, $firmado, $pkeyid,OPENSSL_ALGO_SHA1)) {
+            $sello = base64_encode($firmado);
+        }
+
+        $pdf->SetFont(FONT_AEALARABIYA,'B',7);
+        $pdf->WriteHTMLCell(200,$alto,5,115, "<b>CADENA ORIGINAL:</b>",0,0);
+        $pdf->SetFont(FONT_AEALARABIYA,'',6);
+        $pdf->WriteHTMLCell(200,$alto,5,118, $datos,0,0);
+
+        $pdf->SetFont(FONT_AEALARABIYA,'B',7);
+        $pdf->WriteHTMLCell(200,$alto,5,122, "<b>SELLO DIGITAL:</b>",0,0);
+        $pdf->SetFont(FONT_AEALARABIYA,'',6);
+        $pdf->WriteHTMLCell(200,$alto,5,125, $sello,0,0);
+
+//        $pdf->WriteHTMLCell(195,$alto,5,120, $sello,0,0);
+
+        /*
+        //        $datos = $html;
+                //dd($datos);
+                if (file_exists('/etc/ssl/openssl.cnf')){
+        //            echo "Existe";
+                }
+        //Se deben crear dos claves aparejadas, una clave pública y otra privada
+        //A continuación el array de configuración para la creación del juego de claves
+                $configArgs = array(
+                    'config' => '/etc/ssl/openssl.cnf', //<-- esta ruta es necesaria si trabajas con XAMPP
+                    'private_key_bits' => 8192,
+                    'private_key_type' => OPENSSL_ALGO_SHA1
+                );
+                $resourceNewKeyPair = openssl_pkey_new($configArgs);
+                if (!$resourceNewKeyPair) {
+                    echo 'Puede que tengas problemas con la ruta indicada en el array de configuración "$configArgs" ';
+                    echo openssl_error_string(); //en el caso que la función anterior de openssl arrojará algun error, este sería visualizado gracias a esta línea
+                    exit;
+                }
+        //obtengo del recurso $resourceNewKeyPair la clave publica como un string
+                $details = openssl_pkey_get_details($resourceNewKeyPair);
+                $publicKeyPem = $details['key'];
+        //        echo $publicKeyPem;
+        //obtengo la clave privada como string dentro de la variable $privateKeyPem (la cual es pasada por referencia)
+                if (!openssl_pkey_export($resourceNewKeyPair, $privateKeyPem, NULL, $configArgs)) {
+                    echo openssl_error_string(); //en el caso que la función anterior de openssl arrojará algun error, este sería visualizado gracias a esta línea
+                    exit;
+                }
+        //guardo la clave publica y privada en disco:
+                file_put_contents('private_key.pem', $privateKeyPem);
+                file_put_contents('public_key.pem', $publicKeyPem);
+        //si bien ya tengo cargado el string de la clave privada, lo voy a buscar a disco para verificar que el archivo private_key.pem haya sido correctamente generado:
+                $privateKeyPem = file_get_contents('private_key.pem');
+        //obtengo la clave privada como resource desde el string
+                $resourcePrivateKey = openssl_get_privatekey($privateKeyPem);
+        //crear la firma dentro de la variable $firma (la cual es pasada por referencia)
+                if (!openssl_sign($datos, $firma, $resourcePrivateKey, OPENSSL_ALGO_SHA256)) {
+                    echo openssl_error_string(); //en el caso que la función anterior de openssl arrojará algun error, este sería visualizado gracias a esta línea
+                    exit;
+                }
+        // guardar la firma en disco:
+                //echo $firma;
+                file_put_contents('signature.dat', $firma);
+        // comprobar la firma
+                if (openssl_verify($datos, $firma, $publicKeyPem, 'sha256WithRSAEncryption') === 1) {
+                    $sello =  base64_encode($firma);
+                } else {
+                    //echo 'la firma es invalida y/o los datos fueron alterados';
+                }
+        //echo $sello;
+                $pdf->SetFont(FONT_AEALARABIYA,'B',6);
+                $pdf->WriteHTMLCell(120,$alto,5,110, $sello,0,0);
 
 
-//        $xxxx = openssl_pkcs7_sign(realpath("msg.txt"), "signed.txt",
-//            'file://'.realpath('/home/zarsamco/public_html/eghtesad/certs/zarsamhonar.pem'),
-//            array ('file://'.realpath('/home/zarsamco/public_html/eghtesad/certs/zarsamhonar.pem'), "secretPass"),
-//            array (), PKCS7_NOSIGS
-//        );
-
-// set document signature
-
-        $pdf->WriteHTMLCell(120,$alto,$pdf->getX(),$pdf->getY(),$html,0,0);
-
-        $pdf->SetFont(FONT_AEALARABIYA,'B',6);
-        $pdf->WriteHTMLCell(120,$alto,5,132,"Este documento está firmado digitalmente",0,0);
+        //        $pdf->Image('images/tcpdf_signature.png', $pdf->getX(),$pdf->getY(), 120, $alto, 'PNG');
+        //        $pdf->setSignatureAppearance($pdf->getX(),$pdf->getY(), 120, $alto);
+        //        $pdf->addEmptySignatureAppearance($pdf->getX(),$pdf->getY(), 120, $alto);
 
 
+        */
 
-// define active area for signature appearance
-        $pdf->setSignatureAppearance($pdf->getX(),$pdf->getY(), 120, $alto);
+        //$pdf->set
+//        QrCode::color(255,0,255); //Cambia el color de nuestro codigo
+//        QrCode::backgroundColor(255,255,0); //Le añade el color al background del codigo
+//        QrCode::margin(100);
+//
+//        $pdf->Image(QrCode::generate('http://localhost:8000/imprimir_denuncia/294'), 150,5, 120, 120, 'PNG');
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-// *** set an empty signature appearance ***
-//$pdf->addEmptySignatureAppearance(180, 80, 15, 15);
+//        $pdf->Text(20, 205, 'QRCODE H');
 
-
-        $pdf->Output();
+        $pdf->Output($pdf->FOLIO . '.pdf');
 
     }
 }
